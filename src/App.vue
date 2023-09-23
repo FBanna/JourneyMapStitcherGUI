3100a65d710a375f5937aeb718dfda7c4829700a
FBanna <findlaybannatyne@gmail.com>
2023-09-23 22:11:00 +0100

    begin work on UI

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
   <p>JourneyMapStitcherGUI</p>
   <div class="grid">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
 
-
-
-
-
-    
   </div>
+  <div class="inputsBorder">
+    <div class="inputs">
+      <label for="x">Enter X coord:</label><br>
+      <input type="number" name="x" value="0"><br><br>
+
+      <label for="y">Enter Y coord:</label><br>
+      <input type="number" name="y" value="0"><br><br>
+
+      <button>Goto</button>
+      <br><br>
+      <button>Stitch</button>
+      
+
+    </div>
+  </div>
+
 
 
   
@@ -51,6 +38,22 @@
 
 
 <style>
+  .inputs{
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    width: 180px;
+  }
+
+  .inputsBorder{
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    background-color: #2196F3;
+    width: 220px;
+    height: 100vh;
+  }
+
 
   .grid {
     display: grid;
