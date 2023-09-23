95d8c61ec3f7025e081ffba6a1292a7c0fc082d4
FBanna <findlaybannatyne@gmail.com>
2023-09-23 11:07:44 +0100

    grid test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
   <p>JourneyMapStitcherGUI</p>
+  <div class="grid">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+
+
+
+
+
+    
+  </div>
+
+
+  
 </template>
 
 
 <script setup>
   
 
-  //import { ref, computed } from 'vue'
+  import { ref, computed } from 'vue'
   //import { invoke } from '@tauri-apps/api' 
   //import { convertFileSrc } from '@tauri-apps/api/tauri';
   //import { homeDir, join} from '@tauri-apps/api/path';
@@ -16,5 +51,22 @@
 
 
 <style>
+
+  .grid {
+    display: grid;
+    grid-template-columns: min-content min-content min-content min-content;
+    gap: 1px;
+    background-color: #2196F3;
+    user-select: initial;
+
+  }
+
+  .grid > div {
+    background-color: rgba(255, 255, 255, 0.8);
+    border: 3px solid black;
+    text-align: center;
+    font-size: 5px;
+    user-select: initial;
+  }
 
 </style>
