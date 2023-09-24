d7dc1a279eea767d0f8bf81d2c865dd626bb320a
FBanna <findlaybannatyne@gmail.com>
2023-09-24 20:36:29 +0100

    more GUI additions

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,44 @@
   <div class="inputsBorder">
     <div class="inputs">
 
+      <input type="radio" class="radios" name="type" value="center" v-model="type">
+      <label for="html">center</label><br>
+      <input type="radio" class="radios" name="type" value="span" v-model="type">
+      <label for="css">span</label><br><br>
+
+
+      
+      <div v-if="type == 'center'">
+        <label for="x">Enter X coord:</label><br>
+        <input class="inputsForm" type="number" name="x" value="0"><br><br>
+
+        <label for="y">Enter Y coord:</label><br>
+        <input class="inputsForm" type="number" name="y" value="0"><br><br>
+
+        <label for="radius">Enter radius:</label><br>
+        <input class="inputsForm" type="number" name="radius" max="65024" v-model="radius"><br><br>
+        <div v-if="radius > 65024">
+          IMPUT VALUE LESS THAN 65024     
+        </div>
+
+      </div>
 
 
 
       
-
-      <label for="x">Enter X coord:</label><br>
-      <input class="inputsForm" type="number" name="x" value="0"><br><br>
-
-      <label for="y">Enter Y coord:</label><br>
-      <input class="inputsForm" type="number" name="y" value="0"><br><br>
-
-      <button class="inputsButton">Goto</button>
-      <br><br>
-      <button class="inputsButton">Stitch</button>
       
 
 
 
 
     </div>
+
+    <div class="calls">
+      <button class="inputsButton">Goto</button>
+      <br><br>
+      <button class="inputsButton">Stitch</button>
+    </div>
+
   </div>
 
 
@@ -42,6 +60,8 @@
   //import { convertFileSrc } from '@tauri-apps/api/tauri';
   //import { homeDir, join} from '@tauri-apps/api/path';
   //import { save } from "@tauri-apps/api/dialog";
+  var type = ref("center")
+  var radius = ref(0)
 
 </script>
 
@@ -57,6 +77,36 @@
     height: 20px;
     font-size: 13px;
     transition-duration: 0.2s;
+  }
+
+  .radios[type="radio"]{
+    -webkit-appearance: none;
+    appearance: none;
+    background-color: #fff;
+    margin: 0;
+    width: 11px;
+    height: 11px;
+    border: 2px solid #ae00ff;
+    border-radius: 50%;
+    margin-right: 3px;
+
+    transform: translateY(1px);
+    /*transition: 0.4s ease-in-out 0s;*/
+    background-color: transparent;
+  }
+
+  .radios[type="radio"]:checked{
+    border: 2px solid #e0abfb;
+    box-shadow: 0 0 0 2px #ae00ff;
+    background-color: #ae00ff;
+
+  }
+
+  .calls{
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+    width: 180px;
   }
 
   .inputsButton:hover{
